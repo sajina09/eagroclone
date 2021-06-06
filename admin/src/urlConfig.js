8061222a14ed15904d4e4d9651687caff3c76d93
sajina09 <yourbatman9@gmail.com>
2021-06-06 16:36:51 +0545

    first commit

---
+++ admin/src/urlConfig.js
@@ -0,0 +1,8 @@
+const baseUrl = process.env.API || "http://localhost:2020";
+//const baseUrl = "http://localhost:2000";
+
+export const api = `${baseUrl}/api`;
+
+export const generatePublicUrl = (fileName) => {
+  return `${baseUrl}/public/${fileName}`;
+};
